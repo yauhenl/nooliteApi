eb67f12409f429abed73593343dab0a9a1c7daaf
yauhenl <yauhen.lisimau@gmail.com>
2017-02-20 11:11:02 +0300

    add setBrightness command

--- src/main/kotlin/com/yauhenl/nooliteApi/NooliteApi.kt
+++ src/main/kotlin/com/yauhenl/nooliteApi/NooliteApi.kt
@@ -2,8 +2,12 @@ package com.yauhenl.nooliteApi
 
 import com.yauhenl.nooliteApi.Command.*
 
-class NooliteApi(val host:String) {
-    private fun doAction(channel:Byte, command:Command) = "OK" == khttp.get("http://$host/api.htm", params = hashMapOf("ch" to channel.toString(), "cmd" to command.code.toString())).text
+class NooliteApi(val host: String) {
+    private fun doAction(channel: Byte, command: Command, brightness: Int? = null): Boolean {
+        val params = hashMapOf("ch" to channel.toString(), "cmd" to command.code.toString())
+        if (brightness != null) params.put("br", brightness.toString())
+        return "OK" == khttp.get("http://$host/api.htm", params = params).text
+    }
 
     fun turnOff(channel: Byte) = doAction(channel, turnOff)
     fun slowTurnOff(channel: Byte) = doAction(channel, slowTurnOff)
@@ -11,6 +15,7 @@ class NooliteApi(val host:String) {
     fun slowTurnOn(channel: Byte) = doAction(channel, slowTurnOn)
     fun switch(channel: Byte) = doAction(channel, switch)
     fun revertSlowTurn(channel: Byte) = doAction(channel, revertSlowTurn)
+    fun setBrightness(channel: Byte, brightness: Int) = doAction(channel, setBrightness, brightness)
     fun runScene(channel: Byte) = doAction(channel, runScene)
     fun recordScene(channel: Byte) = doAction(channel, recordScene)
     fun unbind(channel: Byte) = doAction(channel, unbind)
@@ -24,22 +29,9 @@ enum class Command(val code: Int) {
     slowTurnOn(3),
     switch(4),
     revertSlowTurn(5),
+    setBrightness(6),
     runScene(7),
     recordScene(8),
     unbind(9),
     bind(15)
-}
-
-fun main(args: Array<String>) {
-    val api = NooliteApi(args[0])
-    println(api.turnOff(0))
-    println(api.slowTurnOff(0))
-    println(api.turnOn(0))
-    println(api.slowTurnOn(0))
-    println(api.switch(0))
-    println(api.revertSlowTurn(0))
-    println(api.runScene(0))
-    println(api.recordScene(0))
-    println(api.unbind(0))
-    println(api.bind(0))
 }
