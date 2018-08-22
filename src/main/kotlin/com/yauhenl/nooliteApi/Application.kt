bdfd6fa8ec45ee462db82795c39ecdd8c7ee7b5c
yauhenl <yauhen.lisimau@gmail.com>
2018-08-23 00:06:55 +0300

    update libs

--- src/main/kotlin/com/yauhenl/nooliteApi/Application.kt
+++ src/main/kotlin/com/yauhenl/nooliteApi/Application.kt
@@ -4,5 +4,15 @@ fun main(args: Array<String>) {
     val host = args[0]
     val channel:Int = args[1].toInt()
     val command = Command.valueOf(args[2])
+    if (command.code == 6) {
+        val br = args[3]
+        val format = Format.valueOf(args[4])
+        val d0 = args[5].toInt()
+        if (format == Format.rgb) {
+            val d1 = args[6].toInt()
+            val d2 = args[7].toInt()
+            val d3 = args[8].toInt()
+        }
+    }
     NooliteApi(host).doAction(channel, command)
 }
--- src/main/kotlin/com/yauhenl/nooliteApi/NooliteApi.kt
+++ src/main/kotlin/com/yauhenl/nooliteApi/NooliteApi.kt
@@ -4,9 +4,22 @@ import com.yauhenl.nooliteApi.Command.*
 import khttp.get
 
 class NooliteApi(val host: String) {
-    fun doAction(channel: Int, command: Command, brightness: Int? = null): Boolean {
+    fun doAction(
+            channel: Int,
+            command: Command,
+            brightness: Int? = null,
+            fmt: Format? = null,
+            d0: Int? = null,
+            d1: Int? = null,
+            d2: Int? = null,
+            d3: Int? = null): Boolean {
         val params = hashMapOf("ch" to (channel - 1).toString(), "cmd" to command.code.toString())
         if (brightness != null) params.put("br", brightness.toString())
+        if (fmt != null) params.put("fmt", fmt.code.toString())
+        if (d0 != null) params.put("d0", d0.toString())
+        if (d1 != null) params.put("d3", d1.toString())
+        if (d2 != null) params.put("d2", d2.toString())
+        if (d3 != null) params.put("d3", d3.toString())
         return "OK" == get("http://$host/api.htm", params = params).text
     }
 
@@ -21,6 +34,11 @@ class NooliteApi(val host: String) {
     fun recordScene(channel: Int) = doAction(channel, recordScene)
     fun unbind(channel: Int) = doAction(channel, unbind)
     fun bind(channel: Int) = doAction(channel, bind)
+}
+
+enum class Format(val code: Int) {
+    single(1),
+    rgb(3)
 }
 
 enum class Command(val code: Int) {
